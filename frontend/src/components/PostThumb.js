8f7a0804814d7b7d01f3114c7b00a443e3310bf5
Charles de Freitas <charles@defreitas.io>
2020-08-25 17:42:51 +0100

    Fix post by title not replacing all spaces

--- frontend/src/components/PostThumb.js
+++ frontend/src/components/PostThumb.js
@@ -10,7 +10,7 @@ export default function PostPreview({ post }) {
   return (
     <div className="my-6" key={post.id}>
       <div className="content">
-        <Link to={`/blog/${post.title.replace(" ", "-")}`}>
+        <Link to={`/blog/${post.title.replace(/ /g, "-")}`}>
           <p className="title is-3 has-text-link is-capitalized">
             {post.title}
           </p>
