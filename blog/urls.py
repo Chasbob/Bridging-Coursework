9f8c516a10f1402bbd8f770fc62f2addffb08b2a
Charles de Freitas <charles@defreitas.io>
2020-08-25 17:47:03 +0100

    Fix URL matcher

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    re_path(r'^api/blog/(?P<pk>([0-9]|[\w-])+)/?$', views.post_detail),
+    re_path(r'^api/blog/(?P<pk>([0-9]|\S)+)/?$', views.post_detail),
     re_path(r'^api/blog/?$', views.post_list),
 ]
