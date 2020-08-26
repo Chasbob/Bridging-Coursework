4dbceda596931a69d356a1298abe91def5ed9d74
Charles de Freitas <charles@defreitas.io>
2020-08-27 00:01:22 +0100

    Clean up cv migrations

--- cv/migrations/0002_add_types.py
+++ cv/migrations/0002_add_types.py
@@ -4,11 +4,11 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('cv',  '0002_auto_20200826_2221'),
+        ('cv',  '0001_initial'),
     ]
 
     def insertData(apps, schema_editor):
-        ItemType = apps.get_model('cv', 'ItemType')
+        ItemType = apps.get_model('cv', 'ItemCategory')
         ItemType(name = "Education").save()
         ItemType(name = "Projects").save()
         ItemType(name = "Work").save()
