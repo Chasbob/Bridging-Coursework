39de6d516519cebdc1d7b05e4a2d6d0f7a6126d4
Charles de Freitas <charles@defreitas.io>
2020-08-27 16:37:10 +0100

    Add markdown body to Item

--- frontend/src/components/cv/Item.js
+++ frontend/src/components/cv/Item.js
@@ -1,4 +1,6 @@
 import React from "react"
+import ReactMarkdown from "react-markdown"
+import CodeBlock from "../../components/CodeBlock"
 
 export default function Item({ item }) {
   return (
@@ -7,7 +9,14 @@ export default function Item({ item }) {
         <div className="card-header">
           <p className="card-header-title">{item.title}</p>
         </div>
-        <div className="card-content">{item.description}</div>
+        <div className="card-content">
+          <ReactMarkdown
+            className="text has-text-left is-family-primary"
+            renderers={{ code: CodeBlock }}
+          >
+            {item.description}
+          </ReactMarkdown>
+        </div>
       </div>
     </div>
   )
