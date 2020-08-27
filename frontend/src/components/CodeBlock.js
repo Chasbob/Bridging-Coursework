80237c0cb5b33c6ae2963fa14445611c546a7163
Charles de Freitas <charles@defreitas.io>
2020-08-27 16:36:46 +0100

    Convert CodeBlock to functional component

--- frontend/src/components/CodeBlock.js
+++ frontend/src/components/CodeBlock.js
@@ -1,26 +1,16 @@
-import React, { PureComponent } from "react"
-import PropTypes from "prop-types"
+import React from "react"
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
-import { coy } from "react-syntax-highlighter/dist/esm/styles/prism/"
+import { materialOceanic } from "react-syntax-highlighter/dist/esm/styles/prism/"
 
-class CodeBlock extends PureComponent {
-  static propTypes = {
-    value: PropTypes.string.isRequired,
-    language: PropTypes.string,
-  }
-
-  static defaultProps = {
-    language: null,
-  }
-
-  render() {
-    const { language, value } = this.props
-    return (
-      <SyntaxHighlighter language={language} style={coy}>
-        {value}
-      </SyntaxHighlighter>
-    )
-  }
+export default function CodeBlock({ value, language }) {
+  return (
+    <SyntaxHighlighter
+      showLineNumbers={true}
+      showInlineLineNumbers={true}
+      language={language}
+      style={materialOceanic}
+    >
+      {value}
+    </SyntaxHighlighter>
+  )
 }
-
-export default CodeBlock
