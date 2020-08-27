13429fcfc87bf1aec676e41cd9b4e6599f25d57a
Charles de Freitas <charles@defreitas.io>
2020-08-27 11:48:40 +0100

    Add GET CV item by type

--- cv/urls.py
+++ cv/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     re_path(r'^api/cv/$', views.item_list),
     re_path(r'^api/cv/types/$', views.type_list),
+    re_path(r'^api/cv/types/(?P<pk>([0-9])+)/$', views.get_type),
 ]
