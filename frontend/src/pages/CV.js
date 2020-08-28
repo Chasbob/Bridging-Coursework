2b5d6a40e57991d0e9fea54d8332152aae31b18e
Charles de Freitas <charles@defreitas.io>
2020-08-28 15:12:02 +0100

    Update CV to use single swr

--- frontend/src/pages/CV.js
+++ frontend/src/pages/CV.js
@@ -8,23 +8,28 @@ import Item from "../components/cv/Item"
 export default function CV() {
   const toItems = async endpoint => {
     let json = await get(endpoint)
-    return json.map(item => <Item key={item.id} item={item} />)
+    let output = {}
+    types.forEach(element => {
+      output[element.name] = json
+        .filter(item => item.category === element.id)
+        .map(item => <Item key={item.id} item={item} />)
+    })
+    return output
   }
-  const { data: education } = useSWR("/api/cv/types/1/", toItems, {
+  const { data: types } = useSWR("/api/cv/types/", get)
+  const { data: items } = useSWR(types ? "/api/cv/" : null, toItems, {
     initialData: [],
     revalidateOnMount: true,
   })
-  const { data: projects } = useSWR("/api/cv/types/2/", toItems, {
-    initialData: [],
-    revalidateOnMount: true,
-  })
-  const { data: work } = useSWR("/api/cv/types/3/", toItems, {
-    initialData: [],
-    revalidateOnMount: true,
-  })
-  const { data: volunteering } = useSWR("/api/cv/types/4/", toItems, {
-    initialData: [],
-    revalidateOnMount: true,
+  const columns = Object.keys(items).map(col => {
+    return (
+      <div key={col} className="column">
+        <article className="notification is-primary py-3">
+          <p className="title is-family-primary">{col}</p>
+        </article>
+        {items[col]}
+      </div>
+    )
   })
   return (
     <section className="section">
@@ -32,32 +37,7 @@ export default function CV() {
         <title>Important Things - CV</title>
       </Helmet>
       <div className="container">
-        <div className="tile is-ancestor is-vertical">
-          <div className="tile is-parent is-vertical">
-            <article className="tile is-child notification is-primary py-3">
-              <p className="title is-family-primary">Education</p>
-            </article>
-            {education}
-          </div>
-          <div className="tile is-parent is-vertical">
-            <article className="tile is-child notification is-primary py-3">
-              <p className="title is-family-primary">Projects</p>
-            </article>
-            {projects}
-          </div>
-          <div className="tile is-parent is-vertical">
-            <article className="tile is-child notification is-primary py-3">
-              <p className="title is-family-primary">Work</p>
-            </article>
-            {work}
-          </div>
-          <div className="tile is-parent is-vertical">
-            <article className="tile is-child notification is-primary py-3">
-              <p className="title is-family-primary">Volunteering</p>
-            </article>
-            {volunteering}
-          </div>
-        </div>
+        <div className="py-3">{columns}</div>
       </div>
     </section>
   )
