3bb3e4828d51b7cbe38a1e537b80970d0181b5c3
Charles de Freitas <charles@defreitas.io>
2020-08-29 22:43:11 +0100

    Add links

--- frontend/src/components/Social.js
+++ frontend/src/components/Social.js
@@ -1,13 +1,26 @@
 import React from "react"
 
 import { DiGithub } from "react-icons/di"
-import { FaLinkedin, FaTwitter } from "react-icons/fa"
+import { FaLinkedin, FaTwitter, FaBlog, FaScroll } from "react-icons/fa"
 import { AiOutlineMail } from "react-icons/ai"
+import { Link } from "react-router-dom"
 
 export default function Social() {
   return (
     <nav className="panel mt-3">
       <p className="panel-heading is-family-primary">Links</p>
+      <Link className="panel-block is-active" to="/blog">
+        <span className="panel-icon">
+          <FaBlog />
+        </span>
+        Blog
+      </Link>
+      <Link className="panel-block is-active" to="/cv">
+        <span className="panel-icon">
+          <FaScroll />
+        </span>
+        CV
+      </Link>
       <a className="panel-block is-active" href="https://github.com/Chasbob">
         <span className="panel-icon">
           <DiGithub />
